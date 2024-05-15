f7286ba0a6c8eb9bb5fa78e62659981bdf57a894
0xhelton <0xhelton@gmail.com>
2024-05-15 20:17:18 -0300

    :sparkles: feat: create unique User

---
+++ src/main/java/br/com/heltonmuniz/rocket_seat_todo_list/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.heltonmuniz.rocket_seat_todo_list.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+}
