bf739792a20c853784bfc8961cb7e7129dbfb9ed
0xhelton <0xhelton@gmail.com>
2024-05-15 20:30:48 -0300

    :sparkles: feat: hashing password with bcrypt

--- src/main/java/br/com/heltonmuniz/rocket_seat_todo_list/user/UserController.java
+++ src/main/java/br/com/heltonmuniz/rocket_seat_todo_list/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.heltonmuniz.rocket_seat_todo_list.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,10 @@ public class UserController {
             System.out.println("User already exists");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
         }
+
+       var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
