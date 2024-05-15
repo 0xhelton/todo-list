25d5e12c955f5a5ec7aa3e8364087e8d67b10133
0xhelton <0xhelton@gmail.com>
2024-05-15 18:20:30 -0300

    :tada: Initial commit

---
+++ src/main/java/br/com/heltonmuniz/rocket_seat_todo_list/RocketSeatTodoListApplication.java
@@ -0,0 +1,13 @@
+package br.com.heltonmuniz.rocket_seat_todo_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketSeatTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketSeatTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/heltonmuniz/rocket_seat_todo_list/RocketSeatTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.heltonmuniz.rocket_seat_todo_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketSeatTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
